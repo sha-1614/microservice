2eeb71de96a71996621baa248eb9b2b68789cf98
Sha <shagul1614@gmail.com>
2019-11-26 10:52:10 +0530

    Source code of services

---
+++ zuul-gateway/src/main/java/com/gateway/zuul/ZuulGatewayApplication.java
@@ -0,0 +1,43 @@
+package com.gateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.gateway.zuul.filters.ErrorFilter;
+import com.gateway.zuul.filters.PostFilter;
+import com.gateway.zuul.filters.PreFilter;
+import com.gateway.zuul.filters.RouteFilter;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
